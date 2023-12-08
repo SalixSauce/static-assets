2bd4d028186d4e0f3e4a029ef5a74a7e9d9fec95
charlesoller <charlesrello@gmail.com>
2023-12-07 19:39:58 -0500

    after lecture

--- server.js
+++ server.js
@@ -1,35 +1,51 @@
 const http = require('http');
 const fs = require("fs");
 
+function getContentType(filename){
+  const ext = filename.split('.')[1];
+  let contentType = 'text/plain';
+
+  switch (ext) {
+    case 'jpg':
+    case 'jpeg':
+      contentType = 'image/jpeg'
+      break;
+    case 'png':
+      contentType: 'image/png';
+      break;
+    case 'css':
+      contentType = 'text/css'
+      break;
+    default:
+      contentType = 'text/plain'
+      break;
+  }
+}
 
 const server = http.createServer((req, res) => {
   // Your code here
   console.log(`${req.method} ${req.url}`);
 
-  let contentType = '';
-  if(req.url.startsWith('/static')){
-    const reqArr = req.url.split('/');
-    if(reqArr[2] === 'images'){
-      contentType = 'image/jpg'
-    } else if (reqArr[2] === 'css'){
-      contentType === 'text/css'
+  // PHASE 2
+  if(req.method === 'GET' && req.url.startsWith('/static')){
+    const assetPath = req.url.split('/static')[1];
+    try {
+      const resBody = fs.readFileSync('./assets', + assetPath);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', getContentType(assetPath))
+      res.end(resBody)
+    } catch {
+      console.error("Cannot find asset", assetPath, "in assets folder");
+      res.statusCode = 400;
+      return res.end();
     }
   }
-  const fileContents = fs.readFileSync(`.${req.url}`, 'utf-8');
+
+  // PHASE 1
+  const fileContents = fs.readFileSync('./index.html', 'utf-8')
   res.statusCode = 200;
-  res.setHeader('Content-Type', contentType);
+  res.setHeader('Content-Type', 'text/html')
   res.end(fileContents)
-
-  // let reqBody = '';
-  // req.on('data', data => {
-  //   reqBody += data;
-  // })
-
-  // console.log(reqBody);
-  // const fileContents = fs.readFileSync('./index.html', 'utf-8')
-  // res.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/html')
-  // res.end(fileContents)
 });
 
 const port = 5001;
