3be9e5b7d827dccda652019aaeca6c061a928121
charlesoller <charlesrello@gmail.com>
2023-12-07 19:11:52 -0500

    in progress

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const http = require('http');
 const fs = require("fs");
 
+
 const server = http.createServer((req, res) => {
-  // Your code here 
+  // Your code here
+  console.log(`${req.method} ${req.url}`);
+
+  let contentType = '';
+  if(req.url.startsWith('/static')){
+    const reqArr = req.url.split('/');
+    if(reqArr[2] === 'images'){
+      contentType = 'image/jpg'
+    } else if (reqArr[2] === 'css'){
+      contentType === 'text/css'
+    }
+  }
+  const fileContents = fs.readFileSync(`.${req.url}`, 'utf-8');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', contentType);
+  res.end(fileContents)
+
+  // let reqBody = '';
+  // req.on('data', data => {
+  //   reqBody += data;
+  // })
+
+  // console.log(reqBody);
+  // const fileContents = fs.readFileSync('./index.html', 'utf-8')
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/html')
+  // res.end(fileContents)
 });
 
-const port = 5000;
+const port = 5001;
 
 server.listen(port, () => console.log('Server is listening on port', port));
